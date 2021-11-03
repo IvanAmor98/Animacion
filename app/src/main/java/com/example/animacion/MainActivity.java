dce8f10881c2a33d4d9f1521b65ff439c8378311
juacarr <juacarrd@v-espino.es>
2021-11-03 13:50:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/animacion/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.animacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animacion;
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        animacion = (AnimationDrawable)
+                ContextCompat.getDrawable(this,R.drawable.animacion);
+        ImageView vista = new ImageView(this);
+        vista.setBackgroundColor(Color.WHITE);
+        vista.setImageDrawable(animacion);
+        vista.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                animacion.start();
+            }
+        });
+        setContentView(vista);
+    }
+}
